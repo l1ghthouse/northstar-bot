968338179dd0da9e45c6eeb8cbc96e3c32bc7195
L1ghtman2k <aibek.zhylkaidarov@hpe.com>
2022-01-29 13:23:40 -0500

    add a more optimized MaxLifetimeSeconds

--- src/config/config.go
+++ src/config/config.go
@@ -11,5 +11,9 @@ type Config struct {
 	Bot                    bot.Config
 	DB                     storage.Config
 	MaxConcurrentInstances uint `default:"1"`
-	MaxLifetimeSeconds     uint `default:"7200"` // 2 hours
+	MaxLifetimeSeconds     uint `default:"6900"` // 2 hours - 5 minutes, since vultr charges hourly
 }
+
+// MaxLifetimeSeconds could be optimized per cloud provider, depending on the billing cycle.
+// For instance, with vultr, the billing cycle is hourly, so the max lifetime could be set to the increments of
+// 3600(1 hour) minus 5 minutes, since vultr to save some money on the last billed hour. (Thanks to pg9182 for the suggestion)
