e389ef570a0cf96159a7adeecd6e5a30e72db163
l1ghtman2k <aibek.zhylkaidarov@hpe.com>
2022-04-30 18:09:11 -0700

    give database file path

--- src/storage/sqlitedb/sqlitedb.go
+++ src/storage/sqlitedb/sqlitedb.go
@@ -8,10 +8,11 @@ import (
 )
 
 type Config struct {
+	file_path string `default:"northstar-bot.db"`
 }
 
 func NewSqliteDB(config Config) (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(config.file_path), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to sqlite: %w", err)
 	}
