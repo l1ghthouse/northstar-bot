199d7b209451ee6441e37ea69bfd18dbbf6ced14
L1ghtman2k <aibek.zhylkaidarov@hpe.com>
2022-05-06 12:30:37 -0400

    add tracking for spawn.nut

--- src/mod/test_ctf_spawns.go
+++ src/mod/test_ctf_spawns.go
@@ -9,12 +9,26 @@ import (
 type TestCTFSpawns struct{}
 
 func (r TestCTFSpawns) ModParams(ctx context.Context) (string, string, string, string, bool, error) {
-	link := "https://raw.githubusercontent.com/R2Northstar/NorthstarMods/maybe-better-ctf-spawns-pr/Northstar.CustomServers/mod/scripts/vscripts/gamemodes/_gamemode_ctf.nut"
-	f := strings.Split(link, "/")
-	file_path := "/" + f[len(f)-1]
-	wget := fmt.Sprintf("wget %s -O %s \n", link, file_path)
-	dockerArgs := fmt.Sprintf("--mount \"type=bind,source=%s,target=%s,readonly\"", file_path, "/usr/lib/northstar/R2Northstar/mods/Northstar.CustomServers/mod/scripts/vscripts/gamemodes/_gamemode_ctf.nut")
-	return wget, dockerArgs, link, "latest", false, nil
+
+	linkOrigin := "https://raw.githubusercontent.com/R2Northstar/NorthstarMods/maybe-better-ctf-spawns-pr/"
+	fileContainerOrigin := "/usr/lib/northstar/R2Northstar/mods/"
+	files := []string{
+		"Northstar.CustomServers/mod/scripts/vscripts/gamemodes/_gamemode_ctf.nut",
+		"Northstar.CustomServers/mod/scripts/vscripts/mp/spawn.nut",
+	}
+
+	wget := ""
+	dockerArgs := ""
+
+	for _, link := range files {
+		f := strings.Split(link, "/")
+		filePath := "/" + f[len(f)-1]
+		wget += fmt.Sprintf("wget %s -O %s \n", linkOrigin+link, filePath)
+		dockerArgs += fmt.Sprintf("--mount \"type=bind,source=%s,target=%s,readonly\"", filePath, fileContainerOrigin+link)
+		dockerArgs += " "
+	}
+
+	return wget, dockerArgs, "", "latest", false, nil
 }
 
 func (r TestCTFSpawns) EnabledByDefault() bool {
