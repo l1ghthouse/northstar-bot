1beda868c792753bf9b29f2538fc4a6f55aca739
L1ghtman2k <aibek.zhylkaidarov@hpe.com>
2022-11-19 20:32:03 -0500

    1.10.7 for bug fixes

--- src/providers/util/util.go
+++ src/providers/util/util.go
@@ -40,9 +40,9 @@ const NorthstarDedicatedRepo = "ghcr.io/pg9182/"
 var DockerTagRegexp = regexp.MustCompile("^(northstar-dedicated|northstar-dedicated-ci|northstar-dedicated-dev):([a-zA-Z0-9_.-]{1,128})$")
 
 var NorthstarVersions = map[string]DockerVersion{
-	"1.10.3": {
+	"1.10.7": {
 		IsLatest:    true,
-		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.10.3",
+		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.10.7",
 	},
 	"1.9.7": {
 		IsLatest:    false,
