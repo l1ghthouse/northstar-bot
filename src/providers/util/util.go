cfcd1b75f335604c2a6da3cdae0d342c54cb4720
Aibek <35672535+L1ghtman2k@users.noreply.github.com>
2022-12-15 19:14:01 -0500

    Update util.go

--- src/providers/util/util.go
+++ src/providers/util/util.go
@@ -40,37 +40,13 @@ const NorthstarDedicatedRepo = "ghcr.io/pg9182/"
 var DockerTagRegexp = regexp.MustCompile("^(northstar-dedicated|northstar-dedicated-ci|northstar-dedicated-dev):([a-zA-Z0-9_.-]{1,128})$")
 
 var NorthstarVersions = map[string]DockerVersion{
-	"1.10.7": {
+	"1.10.11": {
 		IsLatest:    true,
-		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.10.7",
+		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.10.11",
 	},
 	"1.9.7": {
 		IsLatest:    false,
 		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.9.7",
-	},
-	"1.9.3": {
-		IsLatest:    false,
-		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.9.3",
-	},
-	"1.9.0": {
-		IsLatest:    false,
-		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.9.0",
-	},
-	"1.8.0": {
-		IsLatest:    false,
-		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.8.0",
-	},
-	"1.7.0": {
-		IsLatest:    false,
-		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.7.0",
-	},
-	"1.6.4": {
-		IsLatest:    false,
-		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.6.4",
-	},
-	"1.6.3": {
-		IsLatest:    false,
-		DockerImage: NorthstarDedicatedRepo + "northstar-dedicated:1-tf2.0.11.0-ns1.6.3",
 	},
 }
 
