5b2cd92ed436179affcde5c10a55a458380f03b2
l1ghtman2k <aibek.zhil@gmail.com>
2023-09-19 15:25:06 -0400

    use pugs_ver branch

--- src/mod/rebalanced_lts.go
+++ src/mod/rebalanced_lts.go
@@ -13,14 +13,13 @@ type RebalancedLTS struct {
 const LTSRebalancedRepoOwner = "Dinorush"
 const LTSRebalancedRepoName = "LTSRebalance"
 const LTSRebalancedModName = LTSRebalancedRepoOwner + "." + LTSRebalancedRepoName
-const LTSRebalancedModNameKVFix = LTSRebalancedModName + "_KVFix"
 
 func (r RebalancedLTS) ModParams(ctx context.Context) (string, string, string, string, bool, error) {
 	latestTag, err := latestGithubReleaseTag(ctx, LTSRebalancedRepoOwner, LTSRebalancedRepoName, r.PreRelease)
 	if err != nil {
 		return "", "", "", "", false, err
 	}
-	link := fmt.Sprintf("https://github.com/%s/%s/releases/download/%s/%s_%s.zip", LTSRebalancedRepoOwner, LTSRebalancedRepoName, latestTag, LTSRebalancedModName, latestTag)
+	link := fmt.Sprintf("https://github.com/%s/%s/releases/download/%s/%s_%s_pugs_ver.zip", LTSRebalancedRepoOwner, LTSRebalancedRepoName, latestTag, LTSRebalancedModName, latestTag)
 	builder := strings.Builder{}
 	builder.WriteString(cmdWgetZipBuilder(link, LTSRebalancedModName))
 	builder.WriteString(cmdUnzipBuilderWithDst(LTSRebalancedModName))
